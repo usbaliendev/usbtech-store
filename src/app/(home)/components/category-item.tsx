fa85542b77e0ac3ae4094f7924a3609891dd1dd9
usbangelo <usbangelo@sempreceub.com>
2023-11-09 12:35:53 -0300

    feat: add Categories and CategoryItem component

---
+++ src/app/(home)/components/category-item.tsx
@@ -0,0 +1,40 @@
+import { Badge } from "@/components/ui/badge";
+import { Category } from "@prisma/client";
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  const categoryIcon = {
+    keyboards: <KeyboardIcon size={16} />,
+    monitors: <MonitorIcon size={16} />,
+    headphones: <HeadphonesIcon size={16} />,
+    mousepads: <SquareIcon size={16} />,
+    speakers: <SpeakerIcon size={16} />,
+    mouses: <MouseIcon size={16} />,
+  };
+
+  return (
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
+  );
+};
+
+export default CategoryItem;
