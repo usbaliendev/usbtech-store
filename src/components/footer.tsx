9eac355929521b4b4da69e5dc498b635a92227c7
usbaliendev <usbaliendev@gmail.com>
2024-02-29 14:53:28 -0300

    fix: footer placement fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 flex h-14 w-full flex-col items-center justify-center gap-2">
+    <footer className="mt-10 flex h-14 w-full flex-col items-center justify-center gap-2">
       <h3 className="text-xs sm:text-sm">© 2023 Copyright USBK777 Store</h3>
       <span className="flex items-center gap-1.5 text-xs sm:text-sm">
         Made with <Heart size={16} className="text-blue-500" /> by
