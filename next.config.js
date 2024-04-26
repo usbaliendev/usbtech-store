77b2f16af411eccf7c95b380d7db4a1246e6cfa6
usbaliendev <usbaliendev@gmail.com>
2024-04-26 13:09:29 -0300

    chore: next and prisma schema changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.postimg.cc"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+        port: "",
+      },
+    ],
   },
 };
 
