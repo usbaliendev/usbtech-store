11e67d9ac6cb8ba7d2a2d453a69b8b29bb214cc7
usbaliendev <usbaliendev@gmail.com>
2024-05-15 17:10:06 -0300

    feat: add deals page

---
+++ src/app/ofertas/page.tsx
@@ -0,0 +1,34 @@
+import PageBadge from "@/components/ui/page-badge";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+import { PercentIcon } from "lucide-react";
+
+const DealsPage = async () => {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <PageBadge name="Ofertas" />
+
+      <div className="grid grid-cols-2 gap-8">
+        {deals.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={{
+              ...product,
+              totalPrice: computeProductTotalPrice(product),
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DealsPage;
