2314d8f23fa135d66add317440a9c3ac5344d9fe
usbaliendev <usbaliendev@gmail.com>
2024-05-15 12:34:09 -0300

    fix: env.ts next auth

--- env.d.ts
+++ env.d.ts
@@ -6,7 +6,7 @@ declare namespace NodeJS {
     STRIPE_SECRET_KEY: string;
     STRIPE_WEBHOOK_SECRET_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
-    NEXTAUTH_SECRET: string;
+    NEXT_AUTH_SECRET: string;
     HOST_URL: string;
   }
 }
